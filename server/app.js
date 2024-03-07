32420d030df43c5bb5cf39287508189dc9f4c9ff
Jokaar <elaaboudi.anas1@gmail.com>
2024-03-07 11:53:54 +0100

    first commit

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+require("dotenv/config")
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    return res.json("Hai there.....")
+})
+// user auth routes
+const userRoute = require("./routes/auth");
+app.use("/api/users/", userRoute);
+// Artist links
+    
+
+
+mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true });
+mongoose.connection
+    .once("open", () => console.log("connected"))
+    .on("error", (error) => {
+        console.log(`ERROR :${error}`);
+    })
+app.listen(4000, () => console.log("Listening to port 4000"));
